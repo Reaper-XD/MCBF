dedfde9abd1c6dfa3ac5619c578b828a80fb3338
Reza-XD <94230332+Reaper-XD@users.noreply.github.com>
2021-12-23 15:48:55 +0700

    Update MCBF.py

--- MCBF.py
+++ MCBF.py
@@ -410,14 +410,14 @@ def cek_ingfo(kontol):
     	jenis = ''
     print ' [*] jenis kelamin  : %s '%(jenis)
     try:
-    	r = requests.get('https://graph.facebook.com/%s/friends?limit=50000&access_token=%s'%(idt, mail))
+    	r = requests.get('https://graph.facebook.com/%s/friends?limit=50000&access_token=%s'%(idt, kontol))
         z = json.loads(r.text)
         for i in z['data']:
             id.append(i['id'])
     except:pass
     print ' [*] jumlah teman  : %s'%str(len(id));time.sleep(0.03)
     try:
-    	r = requests.get('https://graph.facebook.com/%s/subscribers?access_token=%s'%(idt, mail))
+    	r = requests.get('https://graph.facebook.com/%s/subscribers?access_token=%s'%(idt, kontol))
         z = json.loads(r.text)
         pengikut = z['summary']['total_count']
     except (KeyError, IOError):
