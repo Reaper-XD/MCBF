79d438ed0e43ef1b7309d78a5b9d09de60c75b45
Reza-XD <94230332+Reaper-XD@users.noreply.github.com>
2022-01-04 00:22:01 +0700

    Update decode.py

--- decode.py
+++ decode.py
@@ -1,14 +1,28 @@
 # module
-import marshal, zlib, base64, os, sys
+import marshal, zlib, base64, os, sys, random
+HJ = "\33[0;32m"
+HT = "\33[30;1m"
+K = "\33[33;1m"
+KT = "\33[1;33m"
+P = "\33[37;1m"
+M = "\33[31;1m"
+BM = "\33[36;1m"
+B = "\33[0;36m"
+HJT = "\33[32;1m"
+U = "\33[35;1m"
+
+my_color = [
+    HJ, HT, K, KT, B, BM, M, HJT, U, P ]
+warna = random.choice(my_color)
 os.system("clear")
 os.system("figlet CODE")
-print "\n\33[30;1m__________________________"
-print "\n\33[1;33m(\33[31;1mI\33[1;33m)\33[32;1mWELCOME TO MY SCRIPT\33[1;33m(\33[31;1mI\33[1;33m)"
-print "\n\33[30;1m__________________________"
+print "%s__________________________"%(K)
+print "%s(%sI%s)%sWELCOME TO MY SCRIPT%s(%sI%s)" %(HT,M,HT,KT,HT,M,HT)
+print "%s__________________________"%(K)
 try:
     r = sys.argv[1]
 except:
-    exit("\33[;0musage : python2 decode.py file.py")
+    exit("%susage : python2 decode.py file.py")%(P)
 
 if not os.path.isfile(r):
     exit("File Tidak Ditemukan!")
