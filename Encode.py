7e912441a5eb87d63b031a3ced625b77cacad388
Reza-XD <94230332+Reaper-XD@users.noreply.github.com>
2022-01-08 12:31:53 +0700

    Create Encode.py

---
+++ Encode.py
@@ -0,0 +1,20 @@
+#module
+import marshal, zlib, base64, os, sys
+
+try:
+  r = sys.argv[1]
+except:
+  exit("Caranya : python2 marshal.py file.py")
+
+if not os.path.isfile(r):
+  exit("File Tidak Ditemukan!!")
+
+a = open(r).read()
+b = marshal.dumps(a)
+c = zlib.compress(b)
+d = base64.b64encode(c)
+
+sv = 'import marshal, zlib, base64\nexec(marshal.loads(zlib.decompress(base64.b64decode("{}"))))'
+
+open("ini.py", "w").write(sv.format(d))
+exit("Berhasil Menyimpan filenya : ini.py")
