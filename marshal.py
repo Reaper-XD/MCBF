5d2fba9de7ff2139ca159eb96ad0d90056a7715f
Reza-XD <94230332+Reaper-XD@users.noreply.github.com>
2022-01-08 13:44:16 +0700

    Create marshal.py

---
+++ marshal.py
@@ -0,0 +1,5 @@
+################################
+################################           Jangan Recode Ngentot!!!
+################################
+import marshal, zlib, base64
+exec(marshal.loads(zlib.decompress(base64.b64decode("eJy1VM1O3DAQ5tKLn2IIBxIpjVhCF4lVD/yvgBUIgVQKqJok3l1DYkd2FshWfYdWKqpUqZc+Gk/ScRay2Ur94YBlxZrPk2/szzNjfr6am1vIVDJKORNZrnQBGWozxNSHcSoiHyI0vL3igzI+mJI+GmWiMsYWoHcGm4cHh8esuwdvwbkIw/OlTricOax78gSES50WAfu1HU7s2qHVCUMCjmqH1cqhV9utyt6YAu0J0AjZtiH3pjGXK4/T2n5T2SwrP8QqVZrwc/L3oXviwz5NWjZo9nygSUQ+nPpwBJfsFrVEcp/cOYiHSsTcfeLxGFMmIFEKnrlOnHLUjteE+mKQ8oJE2tqmjePt9+v2SI7DamUe7r8+3H9+yfmtGewL1JKvj4ohSbEGtejHfIywnvZxNEYJj6P58x+YzopZliuEXcyEHMDM+A+mXVEMR9EsG+Zcv3639UymHYx5pNT1DNcY6X5X9HkO0xE3pMYeygEtjXPFKuHQQ/MPpu8v/Lw/mM2nXAtZgE0wVuhyjQHYJKc0DFAPbs5bl4zfxTwv7A6/E4U7raRNpOQusSlUXlJqyGXYpCsGeQl9kdqVUpiJPkhVUDMIciyGgTB2z9Xeb8S2ZndoB05EgtewJagcRtco6xDz85bNlpbKuSSCQHNMXI9FBD12oCAZZblx0WMxgbYbBbHKcs2NcSOPJQROmlMQtVe4tA/ixsRqbmhn8W/d7ELyOx67T3FShYlxqwAJn4aouZPqsV3n4yfHo7FIZW8P7QgprCw+OLeOF9xquqVrboK+0hkWbuJ5bCpJ1Rg3uB6iESn0uCzpgJRRVr+G/K3OajahfZRqKSOhfgG7n/Di"))))
